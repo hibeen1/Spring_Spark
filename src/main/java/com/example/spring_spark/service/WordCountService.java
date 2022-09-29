95476b620267293d6f138b70987ec59a3922418f
황인빈 <hibeen1@naver.com>
2022-09-29 18:01:27 +0900

    220929 18:00

--- src/main/java/com/example/spring_spark/service/WordCountService.java
+++ src/main/java/com/example/spring_spark/service/WordCountService.java
@@ -17,15 +17,26 @@ public class WordCountService {
     @Autowired
     JavaSparkContext sc;
 
-//    public Map<String, Long> getCount(SparkReq req) {
-//
-//        JavaRDD<ArrayList<String>> words = sc.textFile("src/main/resources/static/40.csv").;
-//
+    public Map<String, Long> getCount(SparkReq req) {
+
+        JavaRDD<String> words = sc.textFile("src/main/resources/static/40.csv");
+        words.take(10).forEach(System.out::println);
+        System.out.println(words.take(1));
+        System.out.println(words.take(1).getClass().getName());
+        String [] temp = words.take(2).get(1).toString().split(",");
+        for (String item : temp) {
+            System.out.println(item);
+        }
+        System.out.println("++++++++++++++++++++++++++++++++++++");
+//        for (String word: words.collect()) {
+//            System.out.println(word);
+//        }
+//        System.out.println("++++++++++++++++++++++++++++++++++++");
 //        System.out.println(words.count());
-////        Map<String, Long> wordCounts = words.countByValue();
+        Map<String, Long> wordCounts = words.countByValue();
 //        System.out.println(words);
-//        return wordCounts;
-//    }
+        return wordCounts;
+    }
 
     public Map<String, Long> getCount(List<String> wordList) {
         JavaRDD<String> words = sc.parallelize(wordList);
